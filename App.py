7f30ea19a0b52625f9c3d31532fa95e4d9f23a4a
Mohsen Fallahnejad <flashfa8@gmail.com>
2021-06-29 16:58:42 +0430

    remove comments

--- App.py
+++ App.py
@@ -2,13 +2,3 @@ import Controller
 
 if __name__ == "__main__":
     Controller.run()
-
-# from mutagen import mp3, File
-
-# file = File('C:/Users/Mohsen-lp/Music/Sohrab Pakzad - Male Man Bash.mp3', easy= True)
-
-# print(file)
-
-# kos = '"C:/Users/Mohsen-lp/Music/Sohrab Pakzad" - Male" Man Bash.mp3"'
-
-# print(kos.replace('"', ''))
